a20f8f750d336cba887bdef65b24f6009c6be028
Jahn Kåre Yankie Øystøl <post@yankie.org>
2017-08-30 12:13:18 +0200

    prosjekt_004 third testet import funk

--- main/main.go
+++ main/main.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"fmt"
+	"github.com/baronoystol/prosjekt_004/packageutil"
+)
+
+func main() {
+	fmt.Println(packageutil.Reverse("eråk nhaj"))
+	fmt.Println(packageutil.MyName)
+}
